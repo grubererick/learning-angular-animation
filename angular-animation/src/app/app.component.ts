d568613c2e3312a7689c519c0146de8189a203f8
Erick Gruber <erickgruber@outlook.com>
2022-09-03 17:39:24 +0200

    Learning Angular Animations

---
+++ angular-animation/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  list = ['Milk', 'Sugar', 'Bread'];
+
+    onAdd(item: string) {
+      this.list.push(item);
+    }
+
+    onDelete(value: string) {}
+}
